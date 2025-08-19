7fca0fb578a18a0a8d8793f2abdcdb72add06e40
Rameen Zeeshan <rameen.zeeshanedu@gmail.com>
2025-08-19 20:55:37 +0400

    Create header.php

---
+++ header.php
@@ -0,0 +1,16 @@
+<?php
+// header.php
+?>
+ 
+<header>
+  <div class="logo" style="font-weight: 700; font-size: 1.5rem; color: #7fffd4; text-shadow: 0 0 10px #40e0d0;">
+    Rehan.Education
+  </div>
+  <nav>
+    <a href="index.php">Home</a>
+    <a href="curriculum.php">Curriculum</a>
+    <a href="facilitators.php">Facilitators</a>
+    <a href="contact.php">Contact Us</a>
+  </nav>
+</header>
+ 
